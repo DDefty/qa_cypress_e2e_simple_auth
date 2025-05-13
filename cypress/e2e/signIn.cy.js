799958ccd2e0764c2e4027993008a88352c0a1f5
DDefty <dawid.kogut99@gmail.com>
2025-05-13 09:42:22 +0200

    test added

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,35 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+describe('Internet page', () => {
+  it('should log in with valid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
 
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('logout').should('be.visible');
   });
 
-  it('', () => {
+  it('shouldnt log in with invalid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
 
+    cy.get('#username').type('tomsmith1');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('Your username is invalid!').should('be.visible');
+  });
+  it('should log out after successful log in', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+    cy.get('#username').type('tomsmith1');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('button', 'Logout').click();
   });
 });
